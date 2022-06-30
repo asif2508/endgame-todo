a0fb88779033b51c16f058882814af43692c6961
asif2508 <47877578+asif2508@users.noreply.github.com>
2022-06-30 23:39:20 +0600

    basic redux setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Todo from './components/Todo/Todo';
+import Completed from './components/Completed/Completed';
+import Calandar from './components/Calandar/Calandar';
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Todo></Todo> }></Route>
+        <Route path='/completed' element={<Completed></Completed>}></Route>
+        <Route path='/calandar' element={<Calandar></Calandar>}></Route>
+      </Routes>
     </div>
   );
 }
